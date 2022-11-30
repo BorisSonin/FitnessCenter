3d523241e67592c25117ab6efd1089ee7f2a4e31
waycleef <khmalikov@gmail.com>
2022-11-30 16:18:41 +0600

    Добавление модели

---
+++ FitnessCenter/DataStore.swift
@@ -0,0 +1,20 @@
+//
+//  DataStore.swift
+//  FitnessCenter
+//
+//  Created by Алишер Маликов on 30.11.2022.
+//
+
+class DataStore {
+    
+    
+    let clubName = [
+        "Gold's Gym",
+        "XFIT Монарх",
+        "Онегинъ",
+        "Клуб Лужники",
+        "Камелот GYM"
+    ]
+    
+    
+}
