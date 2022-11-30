39245b22244ebe131a0f5d557e9a54ba668cb48e
BorisSonin <bsonin85@gmail.com>
2022-11-30 22:02:56 +0300

    Added information about club and developers

---
+++ FitnessCenter/ViewControllers/TeamClubTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TeamClubTableViewController.swift
+//  FitnessCenter
+//
+//  Created by Борис Сонин on 30.11.2022.
+//
+
+import UIKit
+
+class TeamClubTableViewController: UITableViewController {
+    
+    private var teamCLubList = Team.getTeamList()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 80
+        
+    }
+    
+    // MARK: - Table view data source
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        teamCLubList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TeamList", for: indexPath)
+        let team = teamCLubList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = team.fullName
+        content.secondaryText = team.profession
+        content.image = UIImage(systemName: "person.fill")
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
