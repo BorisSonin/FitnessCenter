ded66a2d151f998ce6d310349844abb159fd967c
IgorPrivalikhin <iprivalihin@mail.ru>
2022-11-30 23:46:33 +0300

    Добавлена модель для контактов.

--- FitnessCenter/ContactsModels.swift
+++ FitnessCenter/ContactsModels.swift
@@ -5,23 +5,22 @@
 //  Created by Анастасия Булдакова on 30.11.2022.
 //
 
-import Foundation
 
 struct Contacts {
-    
     let city: String
-    let adress: String
+    var adress: String
     let phone: String
     let workingHours: String
     let support: String
     
-    static func getContacts() -> Contacts {
+    static func getContacts() -> Contacts{
         Contacts(
             city: "Москва",
-            adress: "ул. Садовая 13А",
+            adress: "Новинский бульвар, 16с4",
             phone: "+7 495 675 33 44",
             workingHours: "пн-вс, круглосуточно",
             support: "8 800 345 78 95"
         )
     }
 }
+
--- FitnessCenter/ViewControllers/ContactVC.swift
+++ FitnessCenter/ViewControllers/ContactVC.swift
@@ -17,50 +17,14 @@ class ContactVC: UIViewController {
     @IBOutlet var workHourseLabel: UILabel!
     @IBOutlet var supportPhoneLabel: UILabel!
     
-    //var user = Contacts! нужно передавать через prepare c главного экрана при нажатии на иконку телефона
-    
+    var center = Contacts.getContacts()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        adressLabel.text = "ул. Садовая 13А" // сюда вместо текста через экземпляр модели - текст
-        phoneLabel.text = "+7 495 675 33 44"
-        workHourseLabel.text = "пн-вс, круглосуточно"
-        supportPhoneLabel.text =  "8 800 345 78 95"
+        adressLabel.text = center.adress
+        phoneLabel.text = center.phone
+        workHourseLabel.text = center.workingHours
+        supportPhoneLabel.text =  center.support
         
     }
-    
-
-
 }
-/*
-    struct Contacts {
-        
-        let city: String
-        let adress: String
-        let phone: String
-        let workingHours: String
-        let support: String
-        
-        static func getContacts() -> Contacts {
-            Contacts(
-                city: "Москва",
-                adress: "ул. Садовая 13А",
-                phone: "+7 495 675 33 44",
-                workingHours: "пн-вс, круглосуточно",
-                support: "8 800 345 78 95"
-            )
-        }
-    }
-}
-
-*/
-/*class ContactVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-}
-*/
