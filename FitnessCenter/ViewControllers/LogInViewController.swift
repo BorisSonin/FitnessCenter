0f8609d05a8d0f8d3ad25812eda44ef0f2ce3fa1
waycleef <khmalikov@gmail.com>
2022-12-02 17:46:27 +0600

    Допиливание моих экранов, передача данных с основного экрана

--- FitnessCenter/ViewControllers/LogInViewController.swift
+++ FitnessCenter/ViewControllers/LogInViewController.swift
@@ -8,22 +8,64 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet var logInLogoImageView: UIImageView!
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    // MARK: - Переменные класса
+    var user: User!
+    
+    // MARK: - Overrides Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        logInLogoImageView.image = UIImage(named: "welcome_logo")
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let aboutUserVC = segue.destination as? AboutUserViewController else { return }
+        aboutUserVC.user = user
+        
     }
-    */
 
+    
+    // MARK: - IBActions
+    @IBAction func logInPressed() {
+        guard userNameTextField.text == user.login, passwordTextField.text == user.password else {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password"
+            )
+            return
+        }
+        
+        performSegue(withIdentifier: "showDiary", sender: nil)
+    }
+    
+    @IBAction func forgotRegisterData(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(title: "Oops!", message: "Your name is \"\(user.login)\"")
+        : showAlert(title: "Oops!", message: "Your password is \"\(user.password)\"")
+        
+    }
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+}
+
+//MARK: - Classes Methods
+extension LogInViewController {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
--- FitnessCenter/ViewControllers/MainViewController.swift
+++ FitnessCenter/ViewControllers/MainViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    private let user = User.getUserData()
+    private let programmName = ProgrammName.getProgrammName()
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let tabBarController = segue.destination as? UITabBarController else { return }
+        guard let viewControllers = tabBarController.viewControllers else { return }
+        
+        viewControllers.forEach {
+            if let logInVC = $0 as? LogInViewController {
+                logInVC.user = user
+            } else if let trainingPlanVC = $0 as? TrainingPlanViewController {
+                trainingPlanVC.trainingPlan = programmName
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello world")
+    }
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        
     }
 
 
