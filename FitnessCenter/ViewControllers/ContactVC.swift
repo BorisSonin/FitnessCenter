1951188d91f82d65685bd479a5e439e8d61450a3
IgorPrivalikhin <iprivalihin@mail.ru>
2022-12-04 10:12:36 +0300

    Добавлен обратный звонок. Проверены под все экраны - ок.

--- FitnessCenter/ViewControllers/ContactVC.swift
+++ FitnessCenter/ViewControllers/ContactVC.swift
@@ -15,7 +15,7 @@ class ContactVC: UIViewController {
     @IBOutlet var adressLabel: UILabel!
     @IBOutlet var phoneLabel: UILabel!
     @IBOutlet var workHourseLabel: UILabel!
-    @IBOutlet var supportPhoneLabel: UILabel!
+
     
     var center = Contacts.getContacts()
     
@@ -24,22 +24,20 @@ class ContactVC: UIViewController {
         adressLabel.text = center.adress
         phoneLabel.text = center.phone
         workHourseLabel.text = center.workingHours
-        //supportPhoneLabel.text =  center.support
-        
     }
     
+// MARK: - IBAction
     @IBAction func backCallButton() {
-        showAlert(withTitle: "Дорогой спортсмен!", andMessage: "Наш оператор свяжется с Вами в ближайшее время!")
-        
+        showAlert(withTitle: "Дорогой клиент!", andMessage: "Наш оператор свяжется с Вами в ближайшее время!")
     }
-    
-    
 }
 
+
+// MARK: - Extension
 extension ContactVC {
     private func showAlert(withTitle title: String, andMessage message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+        let okAction = UIAlertAction(title: "Ок", style: .default) { _ in
            // self.textField.text = ""
         }
         alert.addAction(okAction)
