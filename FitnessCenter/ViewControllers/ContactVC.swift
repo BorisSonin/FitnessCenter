5203ea3405c800d2e600ab170150c1865d0f6e96
IgorPrivalikhin <iprivalihin@mail.ru>
2022-12-04 09:58:14 +0300

    + обратный звонок на экран

--- FitnessCenter/ViewControllers/ContactVC.swift
+++ FitnessCenter/ViewControllers/ContactVC.swift
@@ -24,7 +24,25 @@ class ContactVC: UIViewController {
         adressLabel.text = center.adress
         phoneLabel.text = center.phone
         workHourseLabel.text = center.workingHours
-        supportPhoneLabel.text =  center.support
+        //supportPhoneLabel.text =  center.support
         
+    }
+    
+    @IBAction func backCallButton() {
+        showAlert(withTitle: "Дорогой спортсмен!", andMessage: "Наш оператор свяжется с Вами в ближайшее время!")
+        
+    }
+    
+    
+}
+
+extension ContactVC {
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+           // self.textField.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
