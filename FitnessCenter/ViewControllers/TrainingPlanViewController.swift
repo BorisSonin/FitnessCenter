162bfe8eb6be00a3cd9f3f103b6a01b4dd2f8320
waycleef <khmalikov@gmail.com>
2022-12-05 12:28:05 +0600

    Слияние с веткой Igor

--- FitnessCenter/ViewControllers/TrainingPlanViewController.swift
+++ FitnessCenter/ViewControllers/TrainingPlanViewController.swift
@@ -9,12 +9,14 @@ import UIKit
 
 class TrainingPlanViewController: UIViewController {
 
+    @IBOutlet var logoImage: UIImageView!
     @IBOutlet var programmNameLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     var trainingPlan: [ProgrammName]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        logoImage.image = UIImage(named: "welcome_logo")
         programmNameLabel.text = trainingPlan[0].nameProgramm
         descriptionLabel.text = trainingPlan[0].desctiption
     }
